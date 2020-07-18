71423d59d101bd6fe05a9f7b20b055d6ad4cb920
camilo7896 <63164154+camilo7896@users.noreply.github.com>
2020-07-18 08:51:59 -0500

    Update index.php

--- index.php
+++ index.php
@@ -2,8 +2,7 @@
 $nombre = $_POST['nombre'];
 $mail = $_POST['email'];
 $empresa = $_POST['mensaje'];
-$empresa = $_POST['telefono'];
-$empresa = $_POST['tema'];
+
 
 $header = 'From: ' . $mail . " \r\n";
 $header .= "X-Mailer: PHP/" . phpversion() . " \r\n";
